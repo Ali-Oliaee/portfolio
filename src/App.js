2c003e501c6d1de7a03b9ea8402b32556be8240a
ali <alioliaee31@gmail.com>
2022-04-01 23:36:09 +0430

    :lipstick: add mobile header

--- src/App.js
+++ src/App.js
@@ -11,3 +11,5 @@ const App = () => {
 };
 
 export default App;
+
+//https://coolors.co/palette/000000-14213d-fca311-e5e5e5-ffffff
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,9 +1,22 @@
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import { useState } from "react";
+import { useMediaQuery } from "usehooks-ts";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import CallIcon from "@mui/icons-material/Call";
+import HomeIcon from "@mui/icons-material/Home";
+import ArrowCircleDownIcon from "@mui/icons-material/ArrowCircleDown";
+import FeedOutlinedIcon from "@mui/icons-material/FeedOutlined";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import Drawer from "@mui/material/Drawer";
+import ListItem from "@mui/material/ListItem";
+import List from "@mui/material/List";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
 import "./style.scss";
 
-const Header = () => {
+const DesktopHeader = () => {
   const [value, setValue] = useState(0);
 
   const handleChange = (event, newValue) => {
@@ -23,7 +36,7 @@ const Header = () => {
   }
 
   return (
-    <div className="header">
+    <div className="desktop-header">
       <Tabs
         value={value}
         onChange={handleChange}
@@ -40,6 +53,63 @@ const Header = () => {
       </Tabs>
     </div>
   );
+};
+
+const MobileHeader = () => {
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <div className="mobile-header">
+      <IconButton onClick={() => setVisible(true)}>
+        <MenuIcon className="burger-icon" />
+      </IconButton>
+      <Drawer
+        anchor="left"
+        open={visible}
+        onClose={() => setVisible(false)}
+        className="mobile-header-drawer"
+      >
+        <List>
+          <ListItem button>
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText primary="Home" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <InfoOutlinedIcon />
+            </ListItemIcon>
+            <ListItemText primary="About" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <FeedOutlinedIcon />
+            </ListItemIcon>
+            <ListItemText primary="Resume" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <ArrowCircleDownIcon />
+            </ListItemIcon>
+            <ListItemText primary="Portfolio" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <CallIcon />
+            </ListItemIcon>
+            <ListItemText primary="Contact" />
+          </ListItem>
+        </List>
+      </Drawer>
+    </div>
+  );
+};
+
+const Header = () => {
+  const isMobile = useMediaQuery("(max-width: 500px)");
+
+  return isMobile ? <MobileHeader /> : <DesktopHeader />;
 };
 
 export default Header;
