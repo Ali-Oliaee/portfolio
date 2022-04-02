80daa4e4ea319fa94ffee5890395c0cca22c8f97
ali <alioliaee31@gmail.com>
2022-04-02 13:45:39 +0430

    :lipstick: create contact page

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import AboutPage from "../pages/about";
 import HomePage from "../pages/home/";
+import ContactPage from "../pages/contact";
 
 const MainRouter = () => {
   return (
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="/about" element={<AboutPage />} />
+      <Route path="/contact" element={<ContactPage />} />
     </Routes>
   );
 };
