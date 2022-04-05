d510af5c477df4fb4ce65222a0abab9eb2704d91
ali <alioliaee31@gmail.com>
2022-04-05 17:38:16 +0430

    :rocket: add font

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Footer from "./components/footer";
 import Header from "./components/header";
 import MainRouter from "./router";
+import i18n from "i18next";
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="App" dir={i18n.dir()}>
       <Header />
       <MainRouter />
       <Footer />
