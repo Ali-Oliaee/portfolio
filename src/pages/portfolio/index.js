78333ff020635bb2614a8f59592c55741f6da086
ali oliaee <alioliaee31@gmail.com>
2023-12-08 22:07:24 +0330

    remove swiper package

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -1,14 +1,8 @@
-/* eslint-disable import/no-unresolved */
 import { Button } from '@mui/material'
 import { useTranslation } from 'react-i18next'
 import SmoothList from 'react-smooth-list'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { EffectCoverflow, Pagination, Autoplay } from 'swiper'
-import { ProjectCard, PageWrapper } from '../../components'
+import { PageWrapper, ProjectCard } from '../../components'
 import useProjects from '../../hooks/use-projects'
-import 'swiper/css'
-import 'swiper/css/pagination'
-import 'swiper/css/effect-coverflow'
 import './style.scss'
 
 function PortfolioPage() {
@@ -20,38 +14,20 @@ function PortfolioPage() {
       <SmoothList>
         <h1>{t('portfolio')}</h1>
         <div className="container">
-          <Swiper
-            effect="coverflow"
-            grabCursor
-            centeredSlides
-            autoplay={{
-              delay: 2500,
-              disableOnInteraction: false,
-            }}
-            slidesPerView="auto"
-            coverflowEffect={{
-              rotate: 50,
-              stretch: 0,
-              depth: 100,
-              modifier: 1,
-            }}
-            pagination
-            modules={[Autoplay, EffectCoverflow, Pagination]}
-          >
+          <div className="projects-container">
             {projects
               .reverse()
               .map(({ title, description, demo, code, src }) => (
-                <SwiperSlide key={title} className="swiper-slide">
-                  <ProjectCard
-                    title={title}
-                    src={src}
-                    description={description}
-                    demo={demo}
-                    code={code}
-                  />
-                </SwiperSlide>
+                <ProjectCard
+                  title={title}
+                  src={src}
+                  description={description}
+                  demo={demo}
+                  key={title}
+                  code={code}
+                />
               ))}
-          </Swiper>
+          </div>
         </div>
         <div className="center">
           <Button
